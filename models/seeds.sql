a6375ed80d73ec65581622f2c8f6f85b7af74fcf
Nicole Heersink <nheersink8@gmail.com>
2020-01-27 21:24:10 -0700

    update

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,2 +1,3 @@
+
 INSERT INTO npcs (fullname, race, eye_color, hair_color, build, age, personality, profession) VALUES ("Marvarax Morhjas", "Dragonborn", "Black", "Bronze", "Muscular", 45, "Well Spoken", "Blacksmith");
 INSERT INTO buildings (title, purpose, landLord, ofNote) VALUES ("Dragon's Inferno Smithy", "Store", "Marvarax Morhjas", "Quest Location");
