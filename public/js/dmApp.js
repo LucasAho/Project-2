a089b74f7eb8c263025a9e8bfbf3d7f7e336f540
Lucas Aho <aholucas11@gmail.com>
2020-01-29 07:08:59 -0700

    random race btn works

--- public/js/dmApp.js
+++ public/js/dmApp.js
@@ -4,13 +4,19 @@ let newDescript = "";
 let newPerson = "";
 let newProfess = "";
 
-let namesList = ["Alden", "Alec", "Anton", "Arden", "Arlen", "Armand", "Arron", "Augustus", "Avery", "Benedict", "Bennett", "Branden", "Brendon", "Britt", 
+const namesList = [
+  "Alden", "Alec", "Anton", "Arden", "Arlen", "Armand", "Arron", "Augustus", "Avery", "Benedict", "Bennett", "Branden", "Brendon", "Britt", 
 "Broderick", "Carter", "Chadwick", "Chas", "Chet", "Colby", "Cole", "Cordell", "Dalton", "Damien", "Dante", "Darell", "Darius", "Darron", "Darwin", 
 "Dewitt", "Diego", "Dillon", "Dirk", "Domenic", "Donovan", "Dorian", "Dorsey", "Edison", "Elden", "Elvin", "Erich", "Galen", "Garret", "Gaston", "Gavin",
 "German", "Graham", "Hal", "Hank", "Harlan", "Hayden", "Herschel", "Hoyt", "Hunter", "Isaias", "Issac", "Jacinto", "Jarred", "Jonas", "Kendrick", "Keneth",
 "Kennith", "Keven", "Leif", "Lenard", "Lincoln", "Linwood", "Lucius", "Lynwood", "Malcolm", "Malik", "Maxwell", "McKinley", "Merlin", "Merrill", "Michal", 
 "Monty", "Newton", "Nolan", "Porter", "Quinton", "Raphael", "Reid", "Rory", "Scotty", "Shad", "Stanton", "Stefan", "Thaddeus", "Tobias", "Trenton", "Vance", 
-"Walker", "Walton", "Weldon", "Wes", "Weston", "Willian", "Winford", "Wyatt"];
+"Walker", "Walton", "Weldon", "Wes", "Weston", "Willian", "Winford", "Wyatt"
+];
+
+const raceList = [
+  "Dragonborn", "Dwarf", "Elf", "Gnome", "Half-Elf", "Half-Orc", "Halfling", "Human", "Tiefling"
+]
 
 $(document).ready(function() {
   $("#npcBtns").hide();
@@ -33,12 +39,16 @@ $(function() {
   $("#randomName").on("click", event => {
     event.preventDefault();
     newName = namesList[Math.floor(Math.random() * 101)];
-    console.log(newName)
     $("#npcCardTitle").text(newName);
   });
   $("#raceIn").on("click", event => {
     event.preventDefault();
     newRace = $("#raceForm").val().trim();
+    $("#npcRace").text(newRace);
+  });
+  $("#randomRace").on("click", event => {
+    event.preventDefault();
+    newRace = raceList[Math.floor(Math.random() * 10)];
     $("#npcRace").text(newRace);
   });
   $("#descriptIn").on("click", event => {
