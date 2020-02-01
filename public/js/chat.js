7ab7ea53925b10675b4e4fd1c16cadff3ea2007a
Nicole Heersink <nheersink8@gmail.com>
2020-02-01 13:42:09 -0700

    updating chats to posts

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,8 +1,8 @@
-$(function(){
+$(document).ready(function () {
     //create message
     $("#message-submit").on("click", function (event) {
         event.preventDefault();
-        
+
 
         var newMessage = {
             content: $("#message-box").val().trim(),
@@ -11,45 +11,50 @@ $(function(){
 
         console.log(newMessage);
 
-        $.post("/api/chats", newMessage)
-            // On success, run the following code
-            .then(function () {
+        $.ajax("api/chats", {
+            type: "POST",
+            data: newMessage
+        }).then(function () {
 
-                var row = $("<div>");
-                row.addClass("Message");
+            // $.post("/api/chats", newMessage)
+            //     // On success, run the following code
+            //     .then(function () {
 
-                // row.append("<p>" + newMessage.userName + " Messaged: </p>");
-                row.append("<p>" + newMessage.content + "</p>");
-        row.append(
-            "<p>At " +
-            moment(newMessage.created_at).format("h:mma on dddd") +
-            "</p>"
+                    var row = $("<div>");
+                    row.addClass("Message");
+
+                    // row.append("<p>" + newMessage.userName + " Messaged: </p>");
+                    row.append("<p>" + newMessage.content + "</p>");
+                    row.append(
+                        "<p>At " +
+                        moment(newMessage.created_at).format("h:mma on dddd") +
+                        "</p>"
                 $("#message-board").prepend(row));
 
-            });
-
+                // });
+        })
         // Empty each input box by replacing the value with an empty string
         $("#message-box").val("");
     });
 
-    $.get("/api/chats", function(data) {
+    $.get("/api/chats", function (data) {
 
         if (data.length !== 0) {
-    
-        for (var i = 0; i < data.length; i++) {
-    
-            var row = $("<div>");
-            row.addClass("message");
-    
-            row.append("<p>" + data[i].userName + " message.. </p>");
-            row.append("<p>" + data[i].content + "</p>");
-            row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
-    
-            $("#message-board").prepend(row);
-    
+
+            for (var i = 0; i < data.length; i++) {
+
+                var row = $("<div>");
+                row.addClass("message");
+
+                row.append("<p>" + data[i].userName + " message.. </p>");
+                row.append("<p>" + data[i].content + "</p>");
+                row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+
+                $("#message-board").prepend(row);
+
+            }
+
         }
-    
-        }
-    
+
     });
 })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,12 +26,31 @@ module.exports = function (app) {
     });  
   });
 
-  //chat
+  //char
   app.get("/player;id", (req, res) => {
     db.Char.findAll({
     }).then(dbChars => {
       res.render("player", { 
         chars: dbChars
+      });
+    });
+    
+  });
+
+  app.get("/player:id", (req, res) => {
+    db.Post.findAll({
+    }).then(dbPost => {
+      res.render("player", { 
+        Post: dbPost
+      });
+    });
+    
+  });
+  app.get("/dm:id", (req, res) => {
+    db.Post.findAll({
+    }).then(dbPost => {
+      res.render("dm", { 
+        Post: dbPost
       });
     });
     
@@ -47,50 +66,7 @@ module.exports = function (app) {
 
   });
 
-  //chat
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // cms route loads cms.html
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
-  });
-
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
-  });
-
-  // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  });
-
-  //chatkit added sj
-  app.post('/users', (req, res) => {
-    var { username } = req.body
-    chatkit.createUser({
-        id: username,
-        name: username
-      })
-      .then(() => {
-        res.render(`User created: ${username}`)
-        // res.sendStatus(201)
-      })
-      .catch(err => {
-        if (err.error === 'services/chatkit/user_already_exists') {
-          console.log(`User already exists: ${username}`)
-          res.sendStatus(200)
-        } else {
-          res.status(err.status).json(err)
-        }
-      });
-  });
-
-
-
+  
   // // Load example page and pass in an example by id
   // app.get("/npcs/:id", function(req, res) {
   //   db.NPC.findOne({ where: { id: req.params.id } }).then(function(npcs) {
