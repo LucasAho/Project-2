866acf687fefd90a8a2f9a457e6eb4d036b56cf7
Lucas Aho <aholucas11@gmail.com>
2020-02-03 15:36:39 -0700

    moved schema

--- models/seeds.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO npcs (fullname, race, descript, personality, profession) VALUES ("Marvarax Morhjas", "Dragonborn", "Bronze scales, muscular shoulders, small limp", "Well Spoken", "Blacksmith");
